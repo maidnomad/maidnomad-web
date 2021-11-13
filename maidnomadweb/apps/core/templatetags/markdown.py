420070ced19764c6dedb9979f77348ac0ab0e904
Susumu ISHIGAMI <susumu.ishigami@gmail.com>
2021-11-14 05:53:51 +0900

    MarkDownタグを実装し、コンテンツをMarkdownで書けるようにした

---
+++ maidnomadweb/apps/core/templatetags/markdown.py
@@ -0,0 +1,25 @@
+from textwrap import dedent
+
+import bleach
+from django import template
+from bleach_allowlist import markdown_tags
+from markdown import markdown
+
+register = template.Library()
+
+
+@register.tag(name="markdown")
+def do_markdown(parser, token):
+    nodelist = parser.parse(('endmarkdown',))
+    parser.delete_first_token()
+    return MarkdownNode(nodelist)
+
+
+class MarkdownNode(template.Node):
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+    def render(self, context):
+        content = self.nodelist.render(context)
+        content = dedent(content)
+        content_html = markdown(content)
+        return content_html
--- maidnomadweb/maidnomadweb/settings.py
+++ maidnomadweb/maidnomadweb/settings.py
@@ -83,6 +83,9 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+            "builtins": [
+                "apps.core.templatetags.markdown"
+            ],
         },
     },
 ]
