c2cacce95ff326c131f1601098681cb9cf5d840c
Susumu ISHIGAMI <susumu.ishigami@gmail.com>
2021-11-14 19:30:41 +0900

    共通部品とマークダウンの各コンテンツへの当てはめ

--- maidnomadweb/apps/staticpage/urls.py
+++ maidnomadweb/apps/staticpage/urls.py
@@ -1,10 +1,28 @@
-from django.urls import path
+from django.shortcuts import redirect
+from django.urls import path, reverse
 
 from . import views
 
 urlpatterns = [
     path("", views.staticpage("top"), name="top"),
-    path("information/for_maidcafe_info", views.staticpage("for_maidcafe_info"), name="for_maidcafe_info"),
-    path("information/akihabara_nomad_maid", views.staticpage("akihabara_nomad_maid"), name="akihabara_nomad_maid"),
+    path(
+        "information/maidnomad_info",
+        views.staticpage("maidnomad_info"),
+        name="maidnomad_info",
+    ),
+    path(
+        "information/maidcafe_info",
+        views.staticpage("maidcafe_info"),
+        name="maidcafe_info",
+    ),
     path("organization", views.staticpage("organization"), name="organization"),
+    # 旧サイトからの301リダイレクト
+    path(
+        "information/for_maidcafe_info",
+        lambda request: redirect(reverse("maidnomad_info"), permanent=True),
+    ),
+    path(
+        "information/akihabara_nomad_maid",
+        lambda request: redirect(reverse("maidcafe_info"), permanent=True),
+    ),
 ]
